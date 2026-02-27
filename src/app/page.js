60782cbe3c0638ae100aea5afe5696415b110053
barbosah-dotcom <barbosamvitoria014@gmail.com>
2026-02-27 13:48:21 -0300

    aula css

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,9 @@ function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
+      <h2> Aula introdução</h2>
+      <h3>Layout estilizacao</h3>
+      <p>Nesta aula iremos entender o uso do CSS global e local.</p>
     </div>
   );
 }
