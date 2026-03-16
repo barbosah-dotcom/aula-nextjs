3346b4ea61b46df55e1bbf11595df65b8977e1f6
barbosah-dotcom <barbosamvitoria014@gmail.com>
2026-03-16 15:20:19 -0300

    aula16/03

---
+++ src/app/atividades/atv-1/page.jsx
@@ -0,0 +1,10 @@
+import stayles from "./page.module.css";
+  function atividades() {
+    return(
+        <div>
+            <h1>Atividade </h1>
+            <p>Componente básico para rederizar tela com React </p>
+        </div>
+    )
+}
+export default atividades;
